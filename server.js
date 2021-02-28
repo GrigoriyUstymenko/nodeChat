25d40b9ba8a5cc93b1f836d8eea30f4a5536af1c
GrigoriyUstymenko <gromgoriy@gmail.com>
2021-02-28 14:17:25 +0200

    Simple room creating

--- server.js
+++ server.js
@@ -17,18 +17,41 @@ server.listen(8000, () => {
 
 const ws = new WebSocket.Server({ server });
 
+const rooms = new Map([['Chat', new Set(ws.clients)]]);
+
+const roomClients = (roomName, connection) => {
+  if (!rooms.has(roomName)) {
+    rooms.set(roomName, new Set([connection]));
+  }
+  console.log(roomName);
+  console.log(rooms);
+  console.log(rooms.get(roomName).values());
+  return rooms.get(roomName).values();
+};
+
 ws.on('connection', (connection, req) => {
   const ip = req.socket.remoteAddress;
+  rooms.get('Chat').add(connection);
+
   console.log(`Connected ${ip}`);
+
   connection.on('message', message => {
-    console.log('Received: ' + message);
-    for (const client of ws.clients) {
+    const data = JSON.parse(message);
+    const clients = roomClients(data.roomName, connection);
+
+    console.log(`Received: ${data.value}, room: ${data.roomName}`);
+
+
+    for (const client of clients) {
       if (client.readyState !== WebSocket.OPEN) continue;
       if (client === connection) continue;
-      client.send(message);
+      client.send(data.value);
     }
+
   });
+
   connection.on('close', () => {
+    rooms.get('Chat').delete(connection);
     console.log(`Disconnected ${ip}`);
   });
 });
